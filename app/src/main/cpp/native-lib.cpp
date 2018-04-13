67e18749000a9535aa41d629a70b046a387bfb1d
peak3d <pfau@peak3d.de>
2018-04-13 18:21:55 +0200

    Java CryptoSession Check

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,6 +10,8 @@ Java_com_peak3d_msldrm_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::stringstream hello;
 
+#if 0
+
     const uint8_t uuid[] = {0xed,0xef,0x8b,0xa9,0x79,0xd6,0x4a,0xce,0xa3,0xc8,0x27,0xdc,0xd5,0x1d,0x21,0xed};
     AMediaDrm *mediaDrm = AMediaDrm_createByUUID(uuid);
 
@@ -19,7 +21,7 @@ Java_com_peak3d_msldrm_MainActivity_stringFromJNI(
     hello << "SessionId: " << std::string((const char *)sessionId.ptr, sessionId.length) << "Status:" << status << std::endl;
 
     AMediaDrmKeySetId ksid;
-    ksid.ptr = (const uint8_t*)"ksid006ABF89";
+    ksid.ptr = (const uint8_t*)"ksid1F0269A6";
     ksid.length = 12;
 
     status = AMediaDrm_restoreKeys(mediaDrm, &sessionId, &ksid);
@@ -28,12 +30,19 @@ Java_com_peak3d_msldrm_MainActivity_stringFromJNI(
 
     const uint8_t data[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
     uint8_t result[16];
-    char key[14] = {'1','5','2','3','5','4','9','7','2','2','1','5','8', 0};
+    char key[16] = {'1','5','2','3','5','6','6','8','4','8','7','8','2',0,0,0};
+    char hmacKey[16] = {'1','5','2','3','5','6','9','9','9','0','3','7','5',0,0,0};
     uint8_t iv[16] = {1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8};
 
     status = AMediaDrm_encrypt(mediaDrm, &sessionId, "AES/CBC/NoPadding", (uint8_t*)key, iv, data, result, 16);
 
     hello << "Encrypt: Status:" << status << std::endl;
+
+    uint8_t signature[256];
+    size_t signature_size(256);
+    status = AMediaDrm_sign(mediaDrm, &sessionId, "HmacSHA256", (uint8_t*)hmacKey, result, 16, signature, &signature_size);
+
+    hello << "Sign: Status:" << status << " Sig_size:" << signature_size << std::endl;
 
     status = AMediaDrm_removeKeys(mediaDrm, &sessionId);
 
@@ -44,7 +53,7 @@ Java_com_peak3d_msldrm_MainActivity_stringFromJNI(
     hello << "Close: Status:" << status << std::endl;
 
     AMediaDrm_release(mediaDrm);
-
+#endif
     hello << "Finished";
 
     return env->NewStringUTF(hello.str().c_str());
--- app/src/main/java/com/peak3d/msldrm/MainActivity.java
+++ app/src/main/java/com/peak3d/msldrm/MainActivity.java
@@ -1,19 +1,75 @@
 package com.peak3d.msldrm;
 
+import android.media.NotProvisionedException;
+import android.media.ResourceBusyException;
+import android.media.UnsupportedSchemeException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.media.MediaDrm;
+
+import java.util.Arrays;
+import java.util.UUID;
+import java.lang.String;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final UUID WIDEVINE_UUID = new UUID(0xEDEF8BA979D64ACEL, 0xA3C827DCD51D21EDL);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+
+        String hello = new String();
+        byte[] session;
+        byte[] keySetId = "ksid1F0269A6".getBytes();
+        byte[] keyId = "1523566848782".getBytes();
+        byte[] hmacKeyId = "1523569990375".getBytes();
+        byte[] data = "1111111111111111".getBytes();
+        byte[] iv = "1234567812345678".getBytes();
+
+         try {
+             MediaDrm mediaDrm = new MediaDrm(WIDEVINE_UUID);
+
+             try {
+                 session = mediaDrm.openSession();
+                 hello += "SessionId" + Arrays.toString(session) + "\n";
+
+                 MediaDrm.CryptoSession cryptoSession = mediaDrm.getCryptoSession(session, "AES/CBC/NoPadding", "HmacSHA256");
+
+                 mediaDrm.restoreKeys(session, keySetId);
+
+                 byte[] encrypted = cryptoSession.encrypt(keyId, data, iv);
+
+                 hello += "Encrypted: " + Arrays.toString(encrypted) + "\n";
+
+                 byte[] signature = cryptoSession.sign(hmacKeyId, encrypted);
+
+                 hello += "Signature: " + Arrays.toString(signature) + "\n";
+
+                 mediaDrm.removeKeys(session);
+
+                 mediaDrm.closeSession(session);
+
+                 mediaDrm.release();
+
+
+             } catch (NotProvisionedException e) {
+
+             } catch (ResourceBusyException e){
+             }
+         } catch (UnsupportedSchemeException e){}
+
+
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+
+
+        //tv.setText(stringFromJNI());
+        tv.setText(hello);
     }
 
     /**
